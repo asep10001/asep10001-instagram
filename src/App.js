87e86ddb3de0ca3d2b3d5bb2264a9dcb9c074580
asep10001 <asep10001@mail.unpad.ac.id>
2021-01-03 12:29:25 +0700

    adding default margin for first app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello lets build an instagram clone</h1>
+      <h1>Hello lets build an instagram clone</h1>
+
+      <div className="app__header">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+          className="app__headerImage"
+        />
+      </div>
+      {/* Header */}
+
+      {/* posts */}
+      {/* posts */}
     </div>
   );
 }
