60cadbd1067629e8752339b33cac87924a0c8106
asep10001 <asep10001@mail.unpad.ac.id>
2021-01-09 12:14:58 +0700

    adding some new data and files to the upload images

--- src/App.js
+++ src/App.js
@@ -1,40 +1,166 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import db from "./firebase";
+import db, { auth } from "./firebase";
 import Post from "./Post";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
+import ImageUpload from "./ImageUpload";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-  const [posts, setPosts] = useState([
-    // {
-    //   imageUrl:
-    //     "https://www.adweek.com/wp-content/uploads/2017/02/Facebook-Logo.jpg",
-    //   username: "Asep Agus",
-    //   caption: "Wow it works",
-    // },
-    // {
-    //   imageUrl:
-    //     "https://www.cbronline.com/wp-content/uploads/2016/06/linkedin.jpg",
-    //   username: "Asep Agus",
-    //   caption: "Wow it works",
-    // },
-    // {
-    //   imageUrl: "https://miro.medium.com/max/3600/1*6ahbWjp_g9hqhaTDSJOL1Q.png",
-    //   username: "Agus ",
-    //   caption: "Wow it works 3",
-    // },
-  ]);
+  const classes = useStyles();
+  const [posts, setPosts] = useState([]);
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const unsubscribe = db.collection("posts").onSnapshot((snapshot) => {
-      setPosts(snapshot.docs.map((doc) => ({ id: doc.id, post: doc.data() })));
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
     });
 
     return () => {
       unsubscribe();
     };
-  }, [posts]);
+  }, [user, username]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({ id: doc.id, post: doc.data() }))
+        );
+      });
+  }, []);
+
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((err) => alert(err.message));
+
+    setOpen(false);
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => alert(err.message));
+
+    setOpenSignIn(false);
+  };
   return (
     <div className="App">
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ) : (
+        <h3>Log In to Upload</h3>
+      )}
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <img
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt=""
+              className="app__headerImage"
+            />
+          </center>
+          <form action="" className="app___signup">
+            <Input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <Input
+              type="email"
+              placeholder="e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
+
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <img
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt=""
+              className="app__headerImage"
+            />
+          </center>
+          <form action="" className="app___signup">
+            <Input
+              type="email"
+              placeholder="e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button onClick={signIn}>Sign In</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
@@ -42,6 +168,16 @@ function App() {
           className="app__headerImage"
         />
       </div>
+
+      {user ? (
+        <Button onClick={() => auth.signOut()}>Sign Out</Button>
+      ) : (
+        <div className="app__loginContainer">
+          <Button onClick={() => setOpenSignIn(true)}>Sign in</Button>
+          <Button onClick={() => setOpen(true)}>Sign up</Button>
+        </div>
+      )}
+
       {posts.map(({ id, post }) => (
         <Post
           key={id}
